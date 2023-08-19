f510e224a4caaa397fbbf92bda62fb7e471bbcbb
edoka <peter.edoka.242464@unn.edu.ng>
2023-08-19 17:06:50 +0100

    hand detector beta

---
+++ HandDetector.py
@@ -0,0 +1,94 @@
+import cv2
+import mediapipe as mp
+import time
+import math
+import numpy as np
+
+
+class HandDetector():
+    def __init__(self, mode=False, maxHands=2, detectionCon=0.5, trackCon=0.5):
+        self.mode = mode
+        self.maxHands = maxHands
+        self.detectionCon = detectionCon
+        self.trackCon = trackCon
+
+        self.mpHands = mp.solutions.hands
+        # self.hands = self.mpHands.Hands(self.mode,self.maxHands,
+        #                                 self.detectionCon, self.trackCon)
+        self.hands = self.mpHands.Hands()
+        self.mpDraw = mp.solutions.drawing_utils
+        self.tip_ids = [4, 8, 12, 16, 20]
+
+    def find_hands(self, img, draw=True):
+        imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        self.results = self.hands.process(imgRGB)
+
+        # print(results.multi_hand_landmarks)
+
+        if self.results.multi_hand_landmarks:
+            for handLms in self.results.multi_hand_landmarks:
+                if draw:
+                    self.mpDraw.draw_landmarks(img, handLms, self.mpHands.HAND_CONNECTIONS)
+        return img
+    
+    def find_position(self, img, hand_no=0, draw=True):
+        x_list = []
+        y_list = []
+        bbox = []
+        self.lm_list = []
+
+        # lm_list = []
+
+        if self.results.multi_hand_landmarks:
+            main_hand = self.results.multi_hand_landmarks[hand_no]
+                
+            for id, lm in enumerate(main_hand.landmark):
+                # print(id, lm)
+                h, w, c = img.shape
+                cx, cy = int(lm.x * w), int(lm.y * h)
+                # print(id, cx, cy)
+                x_list.append(cx)
+                y_list.append(cy)
+                self.lm_list.append([id, cx, cy])
+
+                if draw:
+                    cv2.circle(img, (cx, cy), 7, (255, 0, 0), cv2.FILLED)
+            xmin, xmax = min(x_list), max(x_list)        
+            ymin, ymax = min(y_list), max(y_list)   
+            bbox = xmin, ymin, xmax, ymax 
+
+            if draw:
+                cv2.rectangle(img, (xmin - 20, ymin - 20), (xmax + 20, ymax - 20),
+                              (0, 255, 0, 2))    
+        return self.lm_list, bbox
+    
+    def fingers_up(self):
+        fingers = []
+        # Thumb
+        if self.lm_list[self.tip_ids[0]][1] > self.lm_list[self.tip_ids[0] - 1][1]:
+            fingers.append(1)
+        else:
+            fingers.append(0)
+        
+        # Fingers
+        for id in range (1, 5):
+            if self.lm_list[self.tip_ids[id]][2] < self.lm_list[self.tip_ids[id] - 2][2]:
+                fingers.append(1)
+            else:
+                fingers.append(0)
+        # total_fingers = fingers.count(1)
+        return fingers
+    
+    def find_distance(self, p1, p2, img, draw=True, r=15, t=3):
+        x1, y1 = self.lm_list[p1][1:]
+        x2, y2 = self.lm_list[p2][1:]
+        cx, cy = (x1 + x2) // 2, (y1 + y2) // 2
+
+        if draw:
+            cv2.line(img, (x1, y1), (x2, y2), (255, 0, 255), t)
+            cv2.circle(img, (x1, y1), r, (255, 0, 255), cv2.FILLED)
+            cv2.circle(img, (x2, y2), r, (255, 0, 255), cv2.FILLED)
+            cv2.circle(img, (cx, cy), r, (0, 0, 255), cv2.FILLED)
+        length = math.hypot(x2 - x1, y2 - y1)
+
+        return length, img, [x1, y1, x2, y2, cx, cy]
